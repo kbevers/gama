3367fb6a4b8b0a9eb111ee31509ee152b27cabd6
Ales Cepek <cepek@gnu.org>
2019-01-17 18:52:15 +0100

    white space clean up

--- lib/gnu_gama/simplified.h
+++ lib/gnu_gama/simplified.h
@@ -3,12 +3,12 @@
   Copyright (C) 2018  Ales Cepek <cepek@gnu.org>
 
   This file is part of the GNU Gama C++ library
-  
+
   GNU Gama is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   GNU Gama is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
